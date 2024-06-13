c3f6a2e1dbcde7d6ba134e1ed1a3b0fb1d1eba69
BrewLA <brewla.design@gmail.com>
2024-06-12 19:26:47 -0700

    new draggable element

--- components/DraggableShape.tsx
+++ components/DraggableShape.tsx
@@ -5,8 +5,6 @@ import React, { useState } from 'react';
 interface DraggableShapeProps {
   initialX: number;
   initialY: number;
-  size: number;
-  fillColor: string;
 }
 
 const DraggableShape: React.FC<DraggableShapeProps> = ({ initialX, initialY, size, fillColor }) => {
@@ -32,8 +30,8 @@ const DraggableShape: React.FC<DraggableShapeProps> = ({ initialX, initialY, siz
   };
 
   return (
-    <svg
-      width={size}
+    <svg 
+    width={size}
       height={size}
       viewBox={`0 0 ${size} ${size}`}
       fill={fillColor}
@@ -49,8 +47,25 @@ const DraggableShape: React.FC<DraggableShapeProps> = ({ initialX, initialY, siz
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
     >
-      <rect width={size} height={size} />
+        <rect x="0.682373" y="0.648682" width="220.514" height="75.0824" rx="7.5" fill="#181819"/>
+        <rect x="0.682373" y="0.648682" width="220.514" height="75.0824" rx="7.5" stroke="#28292B"/>
+        <rect x="18.1824" y="16.1487" width="5.5103" height="44.0824" rx="2.75515" fill="#2C80FF"/>
+        <rect x="31.0398" y="22.1182" width="5.5103" height="32.1434" rx="2.75515" fill="#2C80FF"/>
+        <rect x="43.897" y="18.9038" width="5.5103" height="38.5721" rx="2.75515" fill="#2C80FF"/>
+        <rect x="56.7544" y="16.1487" width="5.5103" height="44.0824" rx="2.75515" fill="#2C80FF"/>
+        <rect x="69.6118" y="24.8733" width="5.5103" height="26.6331" rx="2.75515" fill="#2C80FF"/>
+        <rect x="82.4692" y="16.1487" width="5.5103" height="44.0824" rx="2.75515" fill="#2C80FF"/>
+        <rect x="95.3267" y="22.1182" width="5.5103" height="32.1434" rx="2.75515" fill="#2C80FF"/>
+        <rect x="108.184" y="18.9038" width="5.5103" height="38.5721" rx="2.75515" fill="#2C80FF"/>
+        <rect x="121.042" y="16.1487" width="5.5103" height="44.0824" rx="2.75515" fill="#2C80FF"/>
+        <rect x="133.899" y="24.8733" width="5.5103" height="26.6331" rx="2.75515" fill="#2C80FF"/>
+        <rect x="146.756" y="16.1487" width="5.5103" height="44.0824" rx="2.75515" fill="#2C80FF"/>
+        <rect x="159.614" y="22.1182" width="5.5103" height="32.1434" rx="2.75515" fill="#2C80FF"/>
+        <rect x="172.471" y="18.9038" width="5.5103" height="38.5721" rx="2.75515" fill="#2C80FF"/>
+        <rect x="185.328" y="16.1487" width="5.5103" height="44.0824" rx="2.75515" fill="#2C80FF"/>
+        <rect x="198.186" y="24.8733" width="5.5103" height="26.6331" rx="2.75515" fill="#2C80FF"/>
     </svg>
+
   );
 };
 
